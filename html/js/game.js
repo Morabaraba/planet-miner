30dfc301d22497591d9b185b5ccf834412a0f362
Morabaraba <morabaraba@openhazel.co.za>
2016-06-21 23:45:52 +0000

    recovered game.js from google chrome cache

--- html/js/game.js
+++ html/js/game.js
@@ -69,23 +69,16 @@ function startGame() {
         jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.ALT);
         actionButton = game.input.keyboard.addKey(Phaser.Keyboard.CONTROL);
         
-        
         emitter = game.add.emitter(0, 0, 1000);
 
         emitter.makeParticles('rock');
         emitter.gravity = 200;
         
-        game.scale.onFullScreenChange.add(function () {
-            var wrapper = document.getElementById('wrapper');
-            wrapper.classList.toggle("center");
-            document.getElementById("chat-input").focus(); 
-        })
-        document.body.style.display = "block";
+        game.stage.disableVisibilityChange = true;
+        
+        document.body.style.background = "black";
         document.getElementById("loading-screen").style.display = "none";
         document.getElementById("game-screen").style.display = "block";
-        document.getElementById("chat-input").focus(); 
-        
-        game.stage.disableVisibilityChange = true;
     }
     
 function particleBurst(pointer, offset) {
@@ -98,7 +91,7 @@ function particleBurst(pointer, offset) {
     //  The second gives each particle a 2000ms lifespan
     //  The third is ignored when using burst/explode mode
     //  The final parameter (10) is how many particles will be emitted in this single burst
-    emitter.start(true, 750, null, 10);
+    emitter.start(true, 750, null, 2);
 
 }    
     var speed = 150;
@@ -190,106 +183,3 @@ function particleBurst(pointer, offset) {
         // game.debug.bodyInfo(player, 16, 24);
     
     }
-    
-    var gm = {};
-    gm.players = {};
-    gm.createPlayer = function(msg) {
-        msg = msg || {clientId : chat.config.clientId};
-        if (gm.players[msg.clientId]) return;
-        
-        var player = game.add.sprite(32, 32, 'dude');
-        
-        var style = { font: "16px Arial", fill: "#ffffff"};
-        var text = game.add.text(0, 0, msg.clientId, style);
-        //player.addChild(text);
-        // TODO fix hack
-        player.text = text;
-        
-        player.animations.add('left', [0, 1, 2, 3], 10, true);
-        player.animations.add('turn', [4], 20, true);
-        player.animations.add('right', [5, 6, 7, 8], 10, true);
-        
-        gm.players[msg.clientId] = player;
-        chat.mq.send({type: 'crt'});
-        return player;
-    }
-    
-    gm.movePlayer = function(player, opts) {
-        opts = opts || {};
-        var msg = {
-            type: 'mov',
-            x: player.x,
-            y: player.y,
-        };
-        msg = _.assignIn(msg, opts);
-        chat.mq.send(msg);
-    }
-    
-    gm.actionPlayer = function(msg) {
-        if (!msg) {
-            chat.mq.send({type: 'act'});
-            return;
-        }
-        var player = gm.players[msg.clientId];
-        if (!player) {
-            console.error('no player found', msg);
-            return;
-        }
-        particleBurst(player);
-    }
-    
-    gm.updatePlayer = function(msg) {
-        if (msg.clientId === chat.config.clientId) return;
-        var player = gm.players[msg.clientId];
-        if (!player) {
-            console.error('no player found', msg);
-            return;
-        }
-        if (msg.idle) {
-            var facing = player.animations.name;
-            player.animations.stop();
-            if (facing == 'left')
-            {
-                player.frame = 0;
-            }
-            else
-            {
-                player.frame = 5;
-            }
-        //} else if (player.x === msg.x) {
-            // nothing
-        } else if (player.x < msg.x) {
-            player.animations.play('right');    
-        } else {
-            player.animations.play('left')
-        }
-        player.x = msg.x;
-        player.y = msg.y;
-    }
-
-function gofull() {
-    if (!game.scale) {
-        window.setTimeout(gofull, 500, true);
-        return;
-    }
-    game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
-    //game.scale.fullScreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
-    //game.scale.windowConstraints.right =  'visual';
-    //game.scale.windowConstraints.bottom = 'visual';
-    var wrapper = document.getElementById('game-screen')
-    game.scale.fullScreenTarget = wrapper;
-    // {"right":"layout","bottom":""}
-    if (game.scale.isFullScreen)
-    {
-        game.scale.stopFullScreen();
-    }
-    else
-    {
-        game.scale.startFullScreen(false);
-    }
-
-}
-
-
-var button = document.querySelector('#btn-fullscreen');
-button.addEventListener('click', gofull);
